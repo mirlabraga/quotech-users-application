419072e5fb3b6de7eeabb5a28c1d8e9775fbd15a
Mirla Chucre <mirlabraga@gmail.com>
2021-04-24 14:19:16 +0100

    Implematation structure of the project. Creating component for List of Users.

---
+++ src/components/UsersList/UsersList.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { shallow } from "enzyme";
+import UsersList from "./UsersList";
+
+describe("UsersList", () => {
+  test("matches snapshot", () => {
+    const wrapper = shallow(<UsersList />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
---
+++ src/lib/users.ts
@@ -0,0 +1,45 @@
+const VERSION_API = process.env.VERSION_API || "v1"
+const URL_BASE = process.env.REACT_APP_BACKEND_URL_BASE || `http://localhost:8080/api/${VERSION_API}`;
+
+export interface Metadata {
+  name: string,
+  email: string,
+  role: string
+}
+
+export interface Id {
+  clientId: string,
+  userId: string
+}
+
+export interface User {
+  metadata: Metadata,
+  id: Id
+}
+
+export const fetchUsers = async (): Promise<User[]> => {
+  const response = await fetch(`${URL_BASE}/users`);
+  if (response.ok) {
+    return (await response.json());
+  } else {
+    throw new Error(`Coulnd't fetch users: ${response.text()}. Status: ${response.status}`);
+  }
+}
+
+export const saveUser = async (): Promise<User[]> => {
+  const response = await fetch(`${URL_BASE}/users`);
+  if (response.ok) {
+    return (await response.json());
+  } else {
+    throw new Error(`Coulnd't fetch users: ${response.text()}. Status: ${response.status}`);
+  }
+}
+
+export const deleteUser = async (): Promise<User[]> => {
+  const response = await fetch(`${URL_BASE}/users`);
+  if (response.ok) {
+    return (await response.json());
+  } else {
+    throw new Error(`Coulnd't fetch users: ${response.text()}. Status: ${response.status}`);
+  }
+}
